f872416e776b86604b392b9e6366dac5c82d0388
krgirard33 <ken.girard@gmail.com>
2022-12-09 21:13:41 -0600

    Now uses chores to get list.title & list.task

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries', 'Clean bathroom'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = [
+      {title:"Yesterday's Chores", tasks: ['Start LaunchCode prep work', 'Buy groceries', 'Empty dishwasher']},
+      {title:"Today's Chores", tasks: ['Load dishwasher', 'Finish LaunchCode prep work','Buy groceries you forgot']},
+      {title:"Tomorrow's Chores", tasks: ['Empty dishwasher','Play with LaunchCode practice code','Hardware store']}
+      ]
+   //todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores","Future Chores"];
 
    constructor() { }
 
