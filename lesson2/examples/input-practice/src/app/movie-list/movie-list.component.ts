3fe2a2158a1df68302e50ccf78f94d84b24cb687
krgirard33 <ken.girard@gmail.com>
2022-12-11 01:15:30 -0600

    Finished input exercise. Used solution for error message.

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+  movies = ["Toy Story", "The Shining", "Sleepless in Seattle", "The Martian"];
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  ngOnInit() {}
+
+  addMovie(newTitle: string) {
+      let message = "";
+      if (newTitle === '') {
+         message = 'Please enter a movie title';
+      } else if (this.movies.includes(newTitle)) {
+         message = `${newTitle} is already in movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return message;
+   //  if (newTitle.length > 0) {
+   //    if (!this.movies.includes(newTitle)) {
+   //      this.movies.push(newTitle);
+   //    }
+   //    else {
+   //       message = 'Already entered';
+   //    }
+   //  }
+   //  else {
+   //    message = 'error';
+   //  }
+   //  return message;
+  }
 }
